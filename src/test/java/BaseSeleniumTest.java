3024d81dd7a707a80fe4abdf6f725dc07e742478
Milena Wiśniewska <milena.wisniewska@britenet.com.pl>
2020-10-09 08:06:11 +0200

    Changing Base Selenium Test Before Class --> After Class

--- src/test/java/BaseSeleniumTest.java
+++ src/test/java/BaseSeleniumTest.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
 import java.util.concurrent.TimeUnit;
@@ -8,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 public abstract class BaseSeleniumTest {
     protected WebDriver driver;
 
-    @BeforeTest
+    @BeforeMethod
     public void setUp(){
         System.out.println("Starting the Test");
         String driverPath = "C:\\Users\\BRITENET\\Downloads\\gs-accessing-data-mysql-master\\HealthServiceTests\\src\\main\\resources\\executables\\drivers\\chromedriver.exe";
@@ -18,7 +20,7 @@ public abstract class BaseSeleniumTest {
 
     }
 
-    @AfterTest
+    @AfterMethod
     public void teardown() throws InterruptedException {
         System.out.println("Ending the Test");
         Thread.sleep(5000);
